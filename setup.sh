047d92ba4c6e4d3567bcf8a4a52c4178c1e91ea7
Quillzyy <shaquirearija1@gmail.com>
2024-12-03 12:23:39 +0000

    Create automation installation!

---
+++ setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Update and install necessary packages
+sudo apt update
+sudo apt install -y curl gnupg nginx git unzip
+
+# Install Bun
+curl -fsSL https://bun.sh/install | bash
+
+# Add Bun to PATH
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Verify Bun installation
+bun --version
+
+if [ $? -ne 0 ]; then
+    echo "Bun installation failed"
+    exit 1
+fi
+
+# Set up Nginx configuration
+sudo tee /etc/nginx/sites-available/sahabat-gula <<EOF
+server {
+    listen 80;
+    client_max_body_size 10M;
+    server_name sahabat-gula-dev.us.to;
+    server_name www.sahabat-gula-dev.us.to;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+
+# Enable the Nginx configuration
+sudo ln -s /etc/nginx/sites-available/sahabat-gula /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl restart nginx
+
+# Clone the repository
+if [ ! -d "Cloud-Computing" ]; then
+    git clone https://github.com/Sahabat-Gula-C242-PS390/Cloud-Computing.git
+else
+    echo "Directory Cloud-Computing already exists. Skipping clone."
+fi
+cd Cloud-Computing
+
+# Create a service account key
+cat <<EOF > sa-key.json
+INSERT_YOUR_SERVICE_ACCOUNT_KEY
+EOF
+
+# Set up environment variables
+cat <<EOF > .env
+GCP_PROJECT_ID="sahabat-gula"
+GCP_SA_KEY="sa-key.json"
+EOF
+
+# Install project dependencies
+bun install
+
+# Build the project
+bun run build
+
+# Start the project
+bun run start:prod
